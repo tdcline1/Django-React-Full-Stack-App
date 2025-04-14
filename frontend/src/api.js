00dce612b699a24b310c70c9f06fbdbdec3b8eb0
tdcline1 <tdcline3@gmail.com>
2025-04-14 17:10:02 -0400

    added apiUrl

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
+const apiUrl = "https://ebc7c567-3255-4ecb-8a38-fd55f5fb6284-dev.e1-us-east-azure.choreoapis.dev/django-react/backend/v1"
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
 
 api.interceptors.request.use(
